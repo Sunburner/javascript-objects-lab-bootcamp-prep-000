96a8eee468902a5c6f7ecadd5d25a26d070bc26e
Angelo Cigna <acigna1982@gmail.com>
2017-12-08 19:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var object3 = delete object3[key]
-  return object3
+  var object4 = delete object3[key]
+  return object4
 }
